5502845045eaa261db2f458521f5b82efebbb977
illegal-function <22005328+illegal-function@users.noreply.github.com>
2018-10-07 17:25:06 +0200

    create and delete implemented

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const { MongoClient } = require('mongodb')
+module.exports = (connection, plugin) => {
+  const reject = str => Promise.reject(new Error(str))
+  const collection = () => connection.collection(
+    plugin.collection || 'tournaments'
+  )
 
-MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-  .then(client => {
-    console.log('connected')
-    return client.close()
-  })
+  return {
+    load (id) {
+      return collection()
+        .then(c => c.findOne({ _id: id }))
+        .then(result => result || reject(`Tournament ${id} not found`))
+    },
+    create (id, params) {
+      return Promise.resolve(plugin.onCreate(params))
+        .then(config => {
+          return collection()
+            .then(c => {
+              const doc = {
+                _id: id,
+                state: 0,
+                status: 'active',
+                config,
+              }
+
+              return c.insertOne(doc)
+                .then(r => doc)
+            })
+        })
+    },
+
+    // should only be used for testing
+    delete (id) {
+      return collection()
+        .then(c => c.findOneAndDelete({ _id: id }))
+        .then(r => (
+          r.ok === 1 && r.lastErrorObject.n === 1
+            ? r.value
+            : reject(`Tournament ${id} not found`))
+        )
+    },
+  }
+}
--- test/ok.js
+++ test/ok.js
@@ -1,11 +1,43 @@
-/* global describe it */
+/* global describe it after */
 
-const _db = require('..')
+const TOURNAMENT_ID = '1234'
 
-describe('compiles', () => {
-  it('actually compiles', () => {
-    if (_db) {
-      return 1
+const validator = new (require('fastest-validator'))()
+const connection = require('../lib/connection')
+
+const debug = require('debug')('trn:test')
+
+const app = require('..')(connection, {
+  onCreate (options) {
+    const schema = {
+      teamsize: { type: 'number', positive: true, integer: true, min: 2, max: 3 },
+      signup: { type: 'string', enum: ['solo', 'team', 'any'] },
     }
+
+    const res = validator.validate(options, schema)
+
+    if (res !== true) {
+      debug(res)
+      return Promise.reject(new Error('Validation failed'))
+    }
+
+    return Promise.resolve(options)
+  },
+})
+
+describe('tournaments', () => {
+  it('can create a tournament', () => {
+    return app.create(TOURNAMENT_ID, {
+      teamsize: 2,
+      signup: 'any',
+    })
+  })
+
+  it('can delete a tournament', () => {
+    return app.delete(TOURNAMENT_ID)
+  })
+
+  after(() => {
+    return connection.close()
   })
 })
